50e2cd87ab6af869623f56562c7fe7568d2e1eef
Kinny Tran <kinny.tran@contemi.com.vn>
2019-06-11 10:37:08 +0700

    some refactor

--- app/routes/users.js
+++ app/routes/users.js
@@ -10,12 +10,23 @@ router.get('/', async function(req, res, next) {
   })
   res.json(data)
 });
+// get 1 user
+router.get('/:id', async function(req, res, next) {
+  const id = req.params.id;
+  const {ip,port} = await getService('users');
+  const data = await callService({
+    method:'get',
+    url: `http://${ip}:${port}/users/${id}` 
+  })
+  res.json(data)
+});
 
 async function callService(requestOptions) {
   const response = await axios(requestOptions);
   return response.data;
 };
 async function getService(serviceName) {
+  // this should be in config file not hard code :)
   const response = await axios.get(`http://localhost:3003/find/${serviceName}/1.0/`);
   return response.data;
 };
