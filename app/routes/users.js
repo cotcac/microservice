f221f28815a12fbe71080a971c78530d26af8287
Kinny Tran <kinny.tran@contemi.com.vn>
2019-06-11 10:20:31 +0700

    using microservice from main app

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+const axios = require('axios');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  const {ip,port} = await getService('users');
+  const data = await callService({
+    method:'get',
+    url: `http://${ip}:${port}/` 
+  })
+  res.json(data)
 });
+
+async function callService(requestOptions) {
+  const response = await axios(requestOptions);
+  return response.data;
+};
+async function getService(serviceName) {
+  const response = await axios.get(`http://localhost:3003/find/${serviceName}/1.0/`);
+  return response.data;
+};
 
 module.exports = router;
