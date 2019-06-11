d5dccac067ebfea8fea9b1e7f59c1c2742979765
Kinny Tran <kinny.tran@contemi.com.vn>
2019-06-11 11:23:07 +0700

    user post service

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const axios = require('axios');
+/* GET post listing. */
+router.get('/posts', async function(req, res, next) {
+  const {ip,port} = await getService('posts');
+  const data = await callService({
+    method:'get',
+    url: `http://${ip}:${port}/` 
+  })
+  res.json(data)
 });
+
+async function callService(requestOptions) {
+  const response = await axios(requestOptions);
+  return response.data;
+};
+async function getService(serviceName) {
+  // this should be in config file not hard code :)
+  const response = await axios.get(`http://localhost:3003/find/${serviceName}/1.0/`);
+  return response.data;
+};
 
 module.exports = router;
