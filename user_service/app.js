752681adfa840f1d4c806763f7372fefb130371a
Vuong Tran <vuongtran@Vuongs-MacBook-Pro.local>
2019-11-18 10:57:46 +0700

    auto refresh registry every 30 seconds

--- user_service/app.js
+++ user_service/app.js
@@ -1,48 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const axios = require('axios');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const axios = require("axios");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var schedule = require("node-schedule");
 var app = express();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-console.log('always run first');
-//auto register to service registry.
-const registerService = () => axios.put(`http://localhost:3003/register/users/1.0/3002`).then(r=>{
-  console.log("register to registry service sucess");
-  
-}).catch(e=>{
-  console.log('register service error', e);
-  
-})
-const unregisterService = () => axios.delete(`http://localhost:3003/register/users/1.0/3002`).then(r=>{
-  console.log("delete to registry service sucess");
-  
-}).catch(e=>{
-  console.log('delete service error', e);
-  
-})
+console.log("always run first");
+//auto register to service registry. Every 30s
+const registerService = () =>
+  axios
+    .put(`http://localhost:3003/register/users/1.0/3002`)
+    .then(r => {
+      console.log("register to registry service sucess");
+    })
+    .catch(e => {
+      console.log("register service error", e);
+    });
+const unregisterService = () =>
+  axios
+    .delete(`http://localhost:3003/register/users/1.0/3002`)
+    .then(r => {
+      console.log("delete to registry service sucess");
+    })
+    .catch(e => {
+      console.log("delete service error", e);
+    });
+// auto regisgter when start app
 registerService();
+// refresh the register every 30'
+schedule.scheduleJob("*/30 * * * * *", function() {
+  registerService();
+});
+
 //clean up
-const cleanup = async() =>{
+const cleanup = async () => {
   await unregisterService();
-}
-process.on('uncaughtException', async () =>{
+};
+process.on("uncaughtException", async () => {
   await cleanup();
-})
+});
 // test unregister
 // setTimeout(()=>{
 //   throw new Error('something happen!');
@@ -56,11 +64,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
