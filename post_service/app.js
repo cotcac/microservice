db0efef273dd48a91c391356c020626d5ee44c96
Vuong Tran <vuongtran@Vuongs-MacBook-Pro.local>
2019-11-18 17:03:37 +0700

    minor change

--- post_service/app.js
+++ post_service/app.js
@@ -1,35 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const axios = require('axios');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const axios = require("axios");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var schedule = require("node-schedule");
 var app = express();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-console.log('always run first');
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+console.log("always run first");
 //auto register to service registry.
-const registerService = () => axios.put(`http://localhost:3003/register/posts/1.0/3001`);
-const unregisterService = () => axios.delete(`http://localhost:3003/register/posts/1.0/3001`);
+const registerService = () =>
+  axios.put(`http://localhost:3003/register/posts/1.0/3001`).then(r => {
+    console.log("register success");
+  });
+const unregisterService = () =>
+  axios.delete(`http://localhost:3003/register/posts/1.0/3001`);
+
 registerService();
+schedule.scheduleJob("*/30 * * * * *", function() {
+  registerService();
+});
 //clean up
-const cleanup = async() =>{
+const cleanup = async () => {
   await unregisterService();
-}
-process.on('uncaughtException', async () =>{
+};
+process.on("uncaughtException", async () => {
   await cleanup();
-})
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -39,11 +45,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
